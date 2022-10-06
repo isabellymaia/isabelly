e6291f2a33c771611ce652cf2770eb851a096fc5
isabellymaia <111510101+isabellymaia@users.noreply.github.com>
2022-10-06 10:24:14 -0300

    Create principal.js

---
+++ principal.js
@@ -0,0 +1,15 @@
+introducao-javascript
+├── css
+│   ├── index.css
+│   └── reset.css
+├── js
+│   └── principal.js
+├── img
+├── favicon.ico// Restante do código HTML
+            </section>
+        </main>
+        <!-- Importação dos Javascripts AQUI -->
+        <script src="js/principal.js"></script>
+    </body>
+</html>
+└── index.html<h1>Aparecida Nutrição</h1><h1 class="titulo">Aparecida Nutrição</h1>document.querySelector(".titulo");var titulo = document.querySelector(".titulo");var titulo = document.querySelector(".titulo")titulo.textContent = "Aparecida Nutricionista";
